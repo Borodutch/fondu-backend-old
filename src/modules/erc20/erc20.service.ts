bea3ea45b8fed26d98b558a4758e90e68b989486
Stas N <stas-nepomn@ya.ru>
2021-05-20 21:18:56 +0800

    Generate nanoid to contract files

--- src/modules/erc20/erc20.service.ts
+++ src/modules/erc20/erc20.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { outputFile, readFileSync } from 'fs-extra';
 import Handlebars from 'handlebars';
+import { nanoid } from 'nanoid';
 import { CreateErc20Dto } from './dto/create-erc20.dto';
 
 @Injectable()
@@ -10,6 +11,7 @@ export class Erc20Service {
     const contractTemplate = Handlebars.compile(contractSource.toString());
     const contractOutput = contractTemplate(createErc20Dto);
 
-    return outputFile('./contracts/ERC20.sol', contractOutput);
+    const outputFilename = nanoid();
+    return outputFile(`./contracts/${outputFilename}.sol`, contractOutput);
   }
 }
