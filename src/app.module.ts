fa0bedc50a15869907ca58f51df68ddb5e786890
Stas N <stas-nepomn@ya.ru>
2021-05-20 22:13:38 +0800

    Static serve for contract files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TerminusModule } from '@nestjs/terminus';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { HealthController } from './modules/health/health.controller';
 import { Erc20Module } from './modules/erc20/erc20.module';
@@ -9,10 +10,15 @@ import { Erc721Module } from './modules/erc721/erc721.module';
 import { CompilerModule } from './modules/compiler/compiler.module';
 import { StatsModule } from './modules/stats/stats.module';
 import { ContractModule } from './modules/contract/contract.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'contracts'),
+      exclude: ['api'],
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
